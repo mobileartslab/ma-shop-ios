ba295552265bd125a64e15de7a64bde0a25befa7
Nick Sophinos <nick@wdeanmedical.com>
2023-05-16 01:37:32 -0400

    formatting

--- ma-shop-ios/MainView.swift
+++ ma-shop-ios/MainView.swift
@@ -7,78 +7,64 @@ struct MainView: View {
   @State private var showContent = false
   
   var body: some View {
-      ZStack(alignment: .top) {
-          ScrollView {
-              VStack(spacing: 40) {
-                  
-                  TopBarView()
-                      .padding(.horizontal, 20)
-                  
-                  ForEach(sampleArticles.indices, id: \.self) { index in
-                      
-                      ArticleCardView(category: sampleArticles[index].category, headline: sampleArticles[index].headline, subHeadline: sampleArticles[index].subHeadline, image: sampleArticles[index].image, content: sampleArticles[index].content, isShowContent: $showContent)
-                      
-                          .padding(.horizontal, 20)
-                          .matchedGeometryEffect(id: index, in: nsArticle)
-                          .onTapGesture {
-                              withAnimation(.interactiveSpring(response: 0.35, dampingFraction: 0.65, blendDuration: 0.1)) {
-                                  self.selectedArticleIndex = index
-                                  self.showContent.toggle()
-                              }
-                          }
-                          .frame(height: min(sampleArticles[index].image.size.height/3, 500))
-                  }
+    ZStack(alignment: .top) {
+      ScrollView {
+        VStack(spacing: 40) {
+          TopBarView().padding(.horizontal, 20)
+          ForEach(sampleArticles.indices, id: \.self) { index in
+            ArticleCardView(category: sampleArticles[index].category, headline: sampleArticles[index].headline, subHeadline: sampleArticles[index].subHeadline, image: sampleArticles[index].image, content: sampleArticles[index].content, isShowContent: $showContent)
+                .padding(.horizontal, 20)
+                .matchedGeometryEffect(id: index, in: nsArticle)
+                .onTapGesture {
+                  withAnimation(.interactiveSpring(response: 0.35, dampingFraction: 0.65, blendDuration: 0.1)) {
+                self.selectedArticleIndex = index
+                self.showContent.toggle()
               }
+            }.frame(height: min(sampleArticles[index].image.size.height/3, 500))
           }
-          .opacity(showContent ? 0 : 1)
+        }
+      }.opacity(showContent ? 0 : 1)
           
-          if showContent,
-             let selectedArticleIndex {
-              ArticleCardView(category: sampleArticles[selectedArticleIndex].category, headline: sampleArticles[selectedArticleIndex].headline, subHeadline: sampleArticles[selectedArticleIndex].subHeadline, image: sampleArticles[selectedArticleIndex].image, content: sampleArticles[selectedArticleIndex].content, isShowContent: $showContent)
-                  .matchedGeometryEffect(id: selectedArticleIndex, in: nsArticle)
-                  .ignoresSafeArea()
-          }
-      }
+      if showContent, let selectedArticleIndex {
+        ArticleCardView(category: sampleArticles[selectedArticleIndex].category, headline: sampleArticles[selectedArticleIndex].headline, subHeadline: sampleArticles[selectedArticleIndex].subHeadline, image: sampleArticles[selectedArticleIndex].image, content: sampleArticles[selectedArticleIndex].content, isShowContent: $showContent)
+          .matchedGeometryEffect(id: selectedArticleIndex, in: nsArticle)
+          .ignoresSafeArea()
+        }
+     }
   }
 }
 
+
 struct TopBarView : View {
-    
-    var body: some View {
-        HStack(alignment: .lastTextBaseline) {
-            VStack(alignment: .leading) {
-                Text(getCurrentDate().uppercased())
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-                Text("Today")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-            }
-            
-            Spacer()
-            
-            AvatarView(image: "profile", width: 40, height: 40)
-            
-        }
+  var body: some View {
+    HStack(alignment: .lastTextBaseline) {
+      VStack(alignment: .leading) {
+        Text(getCurrentDate().uppercased()).font(.caption).foregroundColor(.secondary)
+        Text("Today").font(.largeTitle).fontWeight(.heavy)
+      }
+      Spacer()
+      AvatarView(image: "profile", width: 40, height: 40)
     }
+  }
     
-    func getCurrentDate(with format: String = "EEEE, MMM d") -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = format
-        return dateFormatter.string(from: Date())
-    }
+  func getCurrentDate(with format: String = "EEEE, MMM d") -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = format
+    return dateFormatter.string(from: Date())
+  }
 }
 
+
 struct AvatarView: View {
-    let image: String
-    let width: CGFloat
-    let height: CGFloat
+  let image: String
+  let width: CGFloat
+  let height: CGFloat
     
-    var body: some View {
-        Image(image)
-            .resizable()
-            .frame(width: width, height: height)
-            .clipShape(Circle())
-            .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-    }
+  var body: some View {
+    Image(image)
+      .resizable()
+      .frame(width: width, height: height)
+      .clipShape(Circle())
+      .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+  }
 }
